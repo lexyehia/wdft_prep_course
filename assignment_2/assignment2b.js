34866958c46f342bd3059b12bc04524fdb2f5623
Lex Yehia <lex.yehia@icloud.com>
2017-08-08 15:04:09 -0400

    Switch case homework

--- assignment_2/assignment2b.js
+++ assignment_2/assignment2b.js
@@ -1,8 +1,8 @@
 
-//magic eightball
+//magic eightball using If Else control flow
 
 const numberofreplys = 2;
-function ask(){
+function ask() {
 
     // This only results in either a 1 or 2
     const choice = Math.floor(Math.random() * numberofreplys +1);
@@ -30,4 +30,38 @@ function ask(){
 }
 
 ask();
+
+//magic eightball using switch case control flow
+
+function askagain() {
+
+    // This only results in either a 1 or 2
+    const choice = Math.floor(Math.random() * numberofreplys +1);
+
+    let reply = 'You need to make some options using if/else'
+
+    /*
+    write conditional logic here using if, else if, and else
+
+
+    */
+
+    switch(choice) {
+        case 1:
+            reply = 'Random answer 1 :D';
+            console.log(reply);
+            break;
+        case 2:
+            reply = 'Random answer 2 :D :D :D';
+            console.log(reply);
+            break;
+        default:
+            reply = 'Somehow you broke the computer!!';
+            console.log(reply);
+    }
+
+    return console.log(choice);
+}
+
+askagain();
 
